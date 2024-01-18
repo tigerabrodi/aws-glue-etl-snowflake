b0bb2cd886dea88e9e6eb46f967747eca09d9874
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-18 18:49:15 +0100

    cleanup variables

--- terraform/modules/aws-glue-database/variables.tf
+++ terraform/modules/aws-glue-database/variables.tf
@@ -1,14 +1,4 @@
-variable "s3_data_lake_name" {
-  type = string
-}
 
-variable "s3_data_lake_customers_file_name" {
-  type = string
-}
-
-variable "aws_glue_catalog_table" {
-  type = string
-}
 
 variable "aws_glue_database_name" {
   type = string
